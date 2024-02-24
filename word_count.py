6a059ee9f74ee3cd97f6a661cf4563066b473ae9
paescobarl <paescobarl@unal.edu.co>
2024-02-23 20:43:31 -0500

    tercer intento

--- word_count.py
+++ word_count.py
@@ -119,7 +119,7 @@ def create_output_directory(output_directory):
 #
 
 def save_output(output_directory, sequence):
-    with open(output_directory + "/part-0000", "w") as file:
+    with open(output_directory + "/part-00000", "w") as file:
         for key, value in sequence:
             file.write(f"{key}\t{value}\n")
 
